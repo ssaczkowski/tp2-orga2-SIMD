1d1b65321e41927d11b1f4be1705591785259b68
ssaczkows <ssaczkows@outlook.com>
2017-10-29 23:51:23 -0300

    Delete images and add news. Then change method for print matrix, and clean code.

--- c/rw_images.cpp
+++ c/rw_images.cpp
@@ -1,52 +1,69 @@
 #include <stdio.h>
 
 /*
-int leer_rgb (char *archivo, unsigned char *buffer, int filas, int columnas);
-int escribir_rgb (char *archivo, unsigned char *buffer, int filas, int columnas);
+int read_rgb (char *file, unsigned char *buffer, int rows, int columns);
+int write_rgb (char *file, unsigned char *buffer, int rows, int columns);
 */
 
-void printMatrix(unsigned char img[100][114]);
+void printBuffer(unsigned char buffer[][3], int size);
 
 int main()
 {
 	
-	
 
- 	 FILE *pFichero; 
-     unsigned char byte; /* Un char suele ser 8 bits = byte (No siempre)  */ 
-     pFichero=fopen("../img/gopher1.rgb", "r+b"); // Img RGB 100x114
+	// IMAGE 
+ 	FILE *file; 
+ 	file = fopen("../img/gopher1_2x2.rgb", "r+b"); 
+ 	int width = 2;
+ 	int high = 2;
+ 	
+
+ 	
+    //CHANNELS & BUFFER 
+	unsigned char byte_R; 
+    unsigned char byte_G;
+    unsigned char byte_B;
+    unsigned char buffer[width*high][3]; 
      
-     unsigned char img[100][114];
      
-     
-    	for ( int i = 0 ; i < 100 ; i++ ) {
-     		for ( int j = 0 ; j < 114 ; j++ ) {
-   				if(!feof(pFichero)){
-   					printf("En el if del for %d %d",i,j); // for debug
-         			byte=fgetc(pFichero); /* Cada vez que es invocada la funcion fgetc el puntero del fichero se incrementa un byte */ 
-      	 			img[i][j] = byte;
+    	for ( int i = 0 ; i < width ; i++ ) {
+     		for ( int j = 0 ; j < high ; j++ ) {
+   				if(!feof(file)){
+   				
+         			byte_R=fgetc(file); 
+         			byte_G=fgetc(file) + 8;
+         			byte_B=fgetc(file) + 16;
+         			
+         			buffer[i][0]= byte_R;
+         			buffer[i][1]= byte_G;
+         			buffer[i][2]= byte_B;
+         				
 				}
 			}	
 		}
 		
-
-	  fclose(pFichero); 
-	  printMatrix(img);
+		printBuffer(buffer,(width*high));
+		
+		fclose(file); 
 	  
      
 return 0; 
 }
 
 
-void printMatrix(unsigned char img[100][114]){
-	
-		for (int i=0;i < 100;i++){
-			for (int j=0;j < 114;j++)
+void printBuffer(unsigned char buffer[][3], int size){
+		printf("\n\nBUFFER RGB\n\n 0 = R\t1 = G\t2 = B\n\n\n");
+		printf("Image of %d elements per channel.\n\n\n", size);
+		
+		for (int i=0 ; i < size; i++){
+			for (int j=0 ;j < 3 ;j++)
 			{
-				printf("Elemento ( %d , %d ) de matriz imagen: %d ", i,j,img[i][j]);
-			}	
+				printf("(%d,%d)=%d \t\t\t", i,j,buffer[i][j]);
+			}
+			printf("\n");	
 		}
 }
+
 
 /*
 int leer_rgb (char *archivo, unsigned char *buffer, int filas, int columnas){
