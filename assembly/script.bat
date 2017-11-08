6b7cec8c15f6b758cdec79e8b3402b235d681895
ssaczkows <ssaczkows@outlook.com>
2017-11-08 00:24:29 -0300

    Rename function

--- assembly/script.bat
+++ assembly/script.bat
@@ -1,4 +1,4 @@
 nasm -f win32 -o calculate_proportion.o calculate_proportion.asm
-nasm -f win32 -o main.o main.asm
-mingw32-gcc -Wall -o main main.o calculate_proportion.o
-main.exe
+nasm -f win32 -o interpolate.o interpolate.asm
+mingw32-gcc -Wall -o interpolate interpolate.o calculate_proportion.o
+interpolate.exe
